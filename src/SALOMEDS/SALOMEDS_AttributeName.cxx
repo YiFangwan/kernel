e88164488186c3c1772f714b09e84ef74aaaf6ea
srn <srn@opencascade.com>
2005-04-06 04:49:22 +0000

    Removed debug output

--- src/SALOMEDS/SALOMEDS_AttributeName.cxx
+++ src/SALOMEDS/SALOMEDS_AttributeName.cxx
@@ -9,9 +9,7 @@
 
 SALOMEDS_AttributeName::SALOMEDS_AttributeName(const Handle(SALOMEDSImpl_AttributeName)& theAttr)
 :SALOMEDS_GenericAttribute(theAttr)
-{
-    cout <<"Creation of NAME attr : local" << endl;
-}
+{}
 
 SALOMEDS_AttributeName::SALOMEDS_AttributeName(SALOMEDS::AttributeName_ptr theAttr)
 :SALOMEDS_GenericAttribute(theAttr)
