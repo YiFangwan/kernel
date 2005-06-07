fbeb7b45f28ef7e948aacd1c05ef37fc4e66b6d3
adam <adam>
2005-06-07 09:21:51 +0000

    add vtkVersion include

--- src/VTKViewer/VTKViewer_Definitions.h
+++ src/VTKViewer/VTKViewer_Definitions.h
@@ -2,6 +2,8 @@
 #ifndef __VTKViewer_Definitions_h
 #define __VTKViewer_Definitions_h
 
+#include <vtkVersion.h>
+
 #if ((VTK_MAJOR_VERSION == 4)&&(VTK_MINOR_VERSION <= 2))
 #define _VTK_FLOAT_ float
 #else
