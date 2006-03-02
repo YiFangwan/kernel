db6cd21cd37e1a0f6e706d7c91adf3af6eee285d
srn <srn@opencascade.com>
2006-03-02 11:06:28 +0000

    Removed unnecessary dlopen flag

--- src/Container/Container_i.cxx
+++ src/Container/Container_i.cxx
@@ -308,7 +308,7 @@ Engines_Container_i::load_component_Library(const char* componentName)
   
   void* handle;
 #ifndef WNT
-  handle = dlopen( impl_name.c_str() , RTLD_LAZY | RTLD_GLOBAL) ;
+  handle = dlopen( impl_name.c_str() , RTLD_LAZY ) ;
 #else
   handle = dlopen( impl_name.c_str() , 0 ) ;
 #endif
