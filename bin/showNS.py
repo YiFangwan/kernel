efa7f31438cc6cc34d49abd4b06b469eecaeeeb9
abd <abd@opencascade.com>
2006-05-30 07:49:58 +0000

    fix comment in .py filex after insertin copyrights

--- bin/showNS.py
+++ bin/showNS.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright (C) 2005  OPEN CASCADE, CEA, EDF R&D, LEG
 #           PRINCIPIA R&D, EADS CCR, Lip6, BV, CEDRAT
 # This library is free software; you can redistribute it and/or
@@ -16,7 +18,6 @@
 # 
 # See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
 # 
-#!/usr/bin/env python
 
 import orbmodule
 
