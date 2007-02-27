cf607c722423592eca44ccf73ac8d95ab4af140d
ribes <ribes>
2007-02-27 10:44:17 +0000

    Headers OK CORBA includes OK

--- src/DSC/DSC_Basic/ConnectionManager_i.hxx
+++ src/DSC/DSC_Basic/ConnectionManager_i.hxx
@@ -1,12 +1,37 @@
-// André Ribes EDF R&D - 2006
+//  Copyright (C) 2007  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
+//  CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS 
 // 
+//  This library is free software; you can redistribute it and/or 
+//  modify it under the terms of the GNU Lesser General Public 
+//  License as published by the Free Software Foundation; either 
+//  version 2.1 of the License. 
+// 
+//  This library is distributed in the hope that it will be useful, 
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+//  Lesser General Public License for more details. 
+// 
+//  You should have received a copy of the GNU Lesser General Public 
+//  License along with this library; if not, write to the Free Software 
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA 
+// 
+// See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//
+//
+//
+//  File   : ConnectionManager_i.hxx
+//  Author : André RIBES (EDF)
+//  Module : KERNEL
+
 #ifndef _CONNECTION_MANAGER_I_HXX_
 #define _CONNECTION_MANAGER_I_HXX_
 
 #include <iostream>
 #include <map>
-#include "DSC_Engines.hh"
 #include <pthread.h>
+
+#include <SALOMEconfig.h>
+#include CORBA_SERVER_HEADER(DSC_Engines)
 
 /*! \class ConnectionManager_i
  *  \brief This class implements the interface Engines::ConnectionManager.
--- src/DSC/DSC_Basic/PortProperties_i.hxx
+++ src/DSC/DSC_Basic/PortProperties_i.hxx
@@ -1,9 +1,33 @@
-// André Ribes EDF R&D - 2007
+//  Copyright (C) 2007  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
+//  CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS 
 // 
+//  This library is free software; you can redistribute it and/or 
+//  modify it under the terms of the GNU Lesser General Public 
+//  License as published by the Free Software Foundation; either 
+//  version 2.1 of the License. 
+// 
+//  This library is distributed in the hope that it will be useful, 
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+//  Lesser General Public License for more details. 
+// 
+//  You should have received a copy of the GNU Lesser General Public 
+//  License along with this library; if not, write to the Free Software 
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA 
+// 
+// See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//
+//
+//
+//  File   : ConnectionManager_i.hxx
+//  Author : André RIBES (EDF)
+//  Module : KERNEL
+ 
 #ifndef _PORTPROPERTIES_I_HXX_
 #define _PORTPROPERTIES_I_HXX_
 
-#include "SALOME_Ports.hh"
+#include <SALOMEconfig.h>
+#include CORBA_SERVER_HEADER(SALOME_Ports)
 
 class PortProperties_i:
   public virtual POA_Ports::PortProperties
