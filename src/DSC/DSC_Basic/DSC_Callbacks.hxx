3f6e402460648ffe1bcf1591d4c9d307b1d1d97b
ribes <ribes>
2007-02-28 15:30:03 +0000

    Doxygen documentation and english comments.

--- src/DSC/DSC_Basic/DSC_Callbacks.hxx
+++ src/DSC/DSC_Basic/DSC_Callbacks.hxx
@@ -30,19 +30,20 @@
 #include CORBA_SERVER_HEADER(DSC_Engines)
 
 /*! \class DSC_Callbacks
- *  \brief this is an abstract that defines the methods that the component
+ *  \brief This is an abstract class that defines methods that the component
  *  uses to prevent the component user code that the state of the component has changed.
  *  Currently only port's connection modifications are signaled.
  */
 class DSC_Callbacks
 {
   public:
+    DSC_Callbacks() {}
     virtual ~DSC_Callbacks() {}
 
     /*!
-     * \brief This method is used by the component when the number of connection
+     * This method is used by the component when the number of connection
      * on a provides port changes. This information helps the user code to detect
-     * operation on his ports.
+     * operation on its ports.
      * 
      *
      * \param provides_port_name the name of the provides name that has changed.
@@ -54,13 +55,13 @@ class DSC_Callbacks
 				       const Engines::DSC::Message message) = 0;
 
     /*!
-     * \brief This method is used by the component when the number of connection
+     * This method is used by the component when the number of connection
      * on a uses port changes. This information helps the user code to detect
-     * operation on his ports.
+     * operation on its ports.
      * 
      *
      * \param uses_port_name the name of the uses name that has changed.
-     * \param connection_nbr the new sequence representing the uses port.
+     * \param new_uses_port the new sequence representing the uses port.
      * \param message contains informations about the modification of the port.
      */
     virtual void uses_port_changed(const char* uses_port_name,
