0ac192d648f2e2c7f2c34a885dfea0799ad1f630
ribes <ribes>
2007-03-07 13:12:14 +0000

    Doxygen documentation, english comments.

--- src/DSC/DSC_User/Basic/data_short_port_provides.cxx
+++ src/DSC/DSC_User/Basic/data_short_port_provides.cxx
@@ -1,5 +1,28 @@
-// André Ribes EDF R&D - 2006
+//  Copyright (C) 2007  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
+//  CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS 
 // 
+//  This library is free software; you can redistribute it and/or 
+//  modify it under the terms of the GNU Lesser General Public 
+//  License as published by the Free Software Foundation; either 
+//  version 2.1 of the License. 
+// 
+//  This library is distributed in the hope that it will be useful, 
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+//  Lesser General Public License for more details. 
+// 
+//  You should have received a copy of the GNU Lesser General Public 
+//  License along with this library; if not, write to the Free Software 
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA 
+// 
+// See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//
+//
+//
+//  File   : data_short_port_provides.cxx
+//  Author : André RIBES (EDF)
+//  Module : KERNEL
+
 #include "data_short_port_provides.hxx"
 
 data_short_port_provides::data_short_port_provides() {
@@ -15,11 +38,6 @@ data_short_port_provides::put(CORBA::Short data) {
 
 CORBA::Short
 data_short_port_provides::get() {
-  return _val;
-}
-
-CORBA::Short
-data_short_port_provides::get_local() {
   return _val;
 }
 
