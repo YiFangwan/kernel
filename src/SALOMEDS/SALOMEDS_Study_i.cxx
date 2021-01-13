ee3d8658234c651ec28c2d17883d5de301fd370a
Anthony Geay <anthony.geay@edf.fr>
2021-01-13 17:39:24 +0100

    Unsure : To check...

--- src/SALOMEDS/SALOMEDS_Study_i.cxx
+++ src/SALOMEDS/SALOMEDS_Study_i.cxx
@@ -227,6 +227,8 @@ namespace SALOMEDS
     {
       try
       {
+        if(theIOR.empty())
+          return ;
         CORBA::Object_var obj = _orb->string_to_object(theIOR.c_str());
         if ( obj->_non_existent() ) return;
         SALOME::GenericObj_var gobj = SALOME::GenericObj::_narrow(obj);
